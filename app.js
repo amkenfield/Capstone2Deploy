40b16744050a6fa72252d1e75df1f1271b5a1f52
AndrewKenfield <amkenfield@gmail.com>
2022-07-19 10:25:02 -0500

    fixed routing

--- app.js
+++ app.js
@@ -26,11 +26,11 @@ app.use(authenticateJWT);
 // not sure where this should go (if even works...)
 app.use(express.static(path.resolve(__dirname, "./frontend/epic-viewer/build")));
 
-app.use("api/auth", authRoutes);
-app.use("api/users", usersRoutes);
-app.use("api/lines", linesRoutes);
-app.use("api/authors", authorsRoutes);
-app.use("api/works", worksRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/users", usersRoutes);
+app.use("/api/lines", linesRoutes);
+app.use("/api/authors", authorsRoutes);
+app.use("/api/works", worksRoutes);
 
 
 app.use(function(req, res, next) {
